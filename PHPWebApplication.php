aaa3fc96b64f3e50ab35b7b9872ee696f0c55471
Gurusha95 <gurusha.rana@hotmail.com>
2018-09-24 20:36:58 +1000

    New php file

---
+++ PHPWebApplication.php
@@ -0,0 +1,21 @@
+<?php
+
+$id = 100631905;
+$name = "Gurusha";
+$gpa = 3.5;
+$units = $arrayName = array("Software deployment and evolution","Software testing", "Mobile development", "Software Engineering Final year project B" );
+
+echo "VALUES AND THEIR DATA TYPES";
+echo "<br>";
+echo "<br>";
+
+var_dump($id);
+echo "<br>";
+var_dump($name);
+echo "<br>";
+var_dump($gpa);
+echo "<br>";
+var_dump($units);
+echo "<br>";
+
+ ?>
